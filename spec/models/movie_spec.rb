69470a0e4f66cdd6361c52d506c1b69babb16d44
Blake Sergesketter <blakesergesketter@gmail.com>
2023-10-30 10:21:44 -0500

    model validation testing complete.

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe Movie, type: :model do
     it { should belong_to :studio }
     it { should have_many :movie_actors }
     it { should have_many(:actors).through(:movie_actors) }
-
+  end
+  describe "validations" do 
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :creation_year }
+    it { should validate_presence_of :genre }
   end
 end
