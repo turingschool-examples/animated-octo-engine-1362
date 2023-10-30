451144bad55047713f75b64017af330ef2a826c0
Anthea Yur <acyur6@gmail.com>
2023-10-30 10:33:22 -0500

    Create testing for user story 1

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -5,9 +5,30 @@ RSpec.describe "Studio Index Page" do
     test_data
   end
 
-  describe '#initialize' do
-    it 'exists' do
-      expect().to eq()
+  describe '#Studio Index Page - US 1' do
+    it 'when visiting index, all studios including name and location are listed' do
+      visit '/studios'
+      expect(page).to have_content(@studioghibli.name)
+      expect(page).to have_content(@studioghibli.location)
+      expect(page).to have_content(@pixar.name)
+      expect(page).to have_content(@pixar.location)
+    end
+
+    it 'includes each studios movies with title, creation year, and genre listed' do
+      visit '/studios'
+      studioghibli_movies = @studioghibli.movies
+      studioghibli_movies.each do |m|
+        expect(page).to have_content(m.title)
+        expect(page).to have_content(m.creation_year)
+        expect(page).to have_content(m.genre)
+      end
+
+      pixar_movies = @pixar.movies
+      pixar_movies.each do |m|
+        expect(page).to have_content(m.title)
+        expect(page).to have_content(m.creation_year)
+        expect(page).to have_content(m.genre)
+      end
     end
   end
 end
