ef7069e754d52a98eced44ba3e961ddff81c7c4e
Charles Ren <cren04@syr.edu>
2023-10-30 08:42:08 -0700

    DB setup, model relations and spec files

---
+++ db/migrate/20231030152523_create_movies_actors_join_table.rb
@@ -0,0 +1,9 @@
+class CreateMoviesActorsJoinTable < ActiveRecord::Migration[7.0]
+  def change
+    create_table :movie_actors do |t|
+      t.references :movie, null: false, foreign_key: true
+      t.references :actor, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
