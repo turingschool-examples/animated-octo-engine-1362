18c78614ab9cfca9e305e05e149da9b70235f086
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-10-30 10:14:43 -0600

    Implement features and model testing for US 2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,12 @@ class Movie < ApplicationRecord
   belongs_to :studio
   has_many :movie_actors
   has_many :actors, through: :movie_actors
+
+  def actors_youngest_to_oldest
+    actors.order(age: :asc).pluck(:name)
+  end
+
+  def average_age_of_actors
+    actors.average(:age)
+  end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -6,4 +6,37 @@ RSpec.describe Movie, type: :model do
     it {should have_many :movie_actors}
     it {should have_many(:actors).through(:movie_actors)}
   end
+
+  before :each do
+    @studio1 = Studio.create!(name: "Sony", location: "Hollywood")
+    @studio2 = Studio.create!(name: "Disney", location: "Orlando")
+    @movie1 = @studio1.movies.create!(title: "Spiderman", creation_year: "2002", genre: "superhero")
+    @movie2 = @studio2.movies.create!(title: "Frozen", creation_year: "2013", genre: "animation")
+    @movie3 = @studio2.movies.create!(title: "Up", creation_year: "2009", genre: "animation")
+    @actor1 = @movie1.actors.create!(name: "Actor 1", age: "20")
+    @actor2 = @movie1.actors.create!(name: "Actor 2", age: "10")
+    @actor3 = @movie1.actors.create!(name: "Actor 3", age: "30")
+    @actor4 = @movie2.actors.create!(name: "Actor 4", age: "40")
+    @actor5 = @movie2.actors.create!(name: "Actor 5", age: "50")
+    @actor6 = @movie3.actors.create!(name: "Actor 6", age: "60")
+
+    @movie1.actors << @actor6
+ 
+    
+  end
+  describe "#actors_youngest_to_oldest" do
+    it "gives the nanes of actors in a movie in order from youngest to oldest" do
+      expect(@movie1.actors_youngest_to_oldest).to eq(["Actor 2", "Actor 1", "Actor 3", "Actor 6"])
+      expect(@movie2.actors_youngest_to_oldest).to eq(["Actor 4", "Actor 5"])
+      expect(@movie3.actors_youngest_to_oldest).to eq(["Actor 6"])
+    end
+  end
+
+  describe "#average_age_of_actors" do
+    it "gives the average age of actors in a movie" do
+      expect(@movie1.average_age_of_actors).to eq(30)
+      expect(@movie2.average_age_of_actors).to eq(45)
+      expect(@movie3.average_age_of_actors).to eq(60)
+    end
+  end
 end
