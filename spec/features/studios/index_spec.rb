6e1aeab2a556f4ceb74caea84d8964292a888598
Sam Tran <samuelttran@gmail.com>
2023-10-30 09:44:51 -0600

    Change formatting slightly

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "studio index", type: :feature do
     @movie_1 = Movie.create!(title: "Toy Story", creation_year: "1995", genre: "animation", studio: @studio_1)
     @movie_2 = Movie.create!(title: "Tangled", creation_year: "2000", genre: "animation", studio: @studio_1)
     @movie_3 = Movie.create!(title: "Babe", creation_year: "1997", genre: "farm", studio: @studio_2)
-    @movie_4 = Movie.create!(title: "IDK", creation_year: "1975", genre: "mystert", studio: @studio_2)
+    @movie_4 = Movie.create!(title: "IDK", creation_year: "1975", genre: "mystery", studio: @studio_2)
   end
 
   # Story 1 - Studio Index
