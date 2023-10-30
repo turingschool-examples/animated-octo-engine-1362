7f193cc37defec7d2826bd4138db3debdb3de32b
Anthea Yur <acyur6@gmail.com>
2023-10-30 11:18:09 -0500

    Add model testing for 100 percent coverage

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -1,8 +1,19 @@
 require "rails_helper"
 
 RSpec.describe Actor, type: :model do
+  before :each do
+    test_data
+  end
+
   describe "relationships" do
     it { should have_many :movie_actors}
     it { should have_many(:movies).through(:movie_actors)}
   end
+
+  describe '#self.sort_asc_age' do
+  it 'will resort actors by age from youngest to oldest' do
+    expected_order = [@sootsprite, @mini_totoro, @catbus]
+    expect(@totoro.actors.sort_asc_age).to eq(expected_order)
+  end
+end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,9 +1,21 @@
 require "rails_helper"
 
 RSpec.describe Movie, type: :model do
+  before :each do
+    test_data
+  end
+
   describe "relationships" do
     it {should belong_to :studio}
     it { should have_many :movie_actors}
     it { should have_many(:actors).through(:movie_actors)}
   end
+
+  describe '#average_age' do
+    it 'can return the average age of actors per movie' do
+      average_age = @totoro.actors.pluck(:age).sum.div(@totoro.actors.count)
+      expect(@totoro.average_age.to_i).to eq(average_age)
+    end
+  end
+
 end
