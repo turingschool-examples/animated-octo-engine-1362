e483e7aee7f01b294afefc4f747024bd49dbde1c
Sooyung Kim <kimx1279@gmail.com>
2023-10-30 09:33:10 -0700

    US 2 add model test

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,15 +1,8 @@
 require "rails_helper"
 
 RSpec.describe Movie, type: :model do
-  describe "relationships" do
-    it {should belong_to :studio}
-    it {should have_many :movie_actors}
-    it {should have_many(:actors).through(:movie_actors)}
-  end
-
-  describe 'actors_list' do
-    it 'lists the actors from youngest to oldest' do
-      @us = Studio.create!(name: 'Universal Studio', location: 'Hollywood')
+  before :each do
+    @us = Studio.create!(name: 'Universal Studio', location: 'Hollywood')
 
       @marry = @us.movies.create!(title: 'Marry Me', creation_year: '2022', genre: 'Romance')
   
@@ -22,8 +15,23 @@ RSpec.describe Movie, type: :model do
       MovieActor.create!(movie: @marry, actor: @owen)
       MovieActor.create!(movie: @marry, actor: @sarah)
       MovieActor.create!(movie: @marry, actor: @chloe)
+  end
+  
+  describe "relationships" do
+    it {should belong_to :studio}
+    it {should have_many :movie_actors}
+    it {should have_many(:actors).through(:movie_actors)}
+  end
 
+  describe 'actors_list' do
+    it 'lists the actors from youngest to oldest' do
       expect(@marry.actors_list).to eq([@chloe, @sarah, @jenni, @owen])
+    end
+  end
+
+  describe 'ave_actors_age' do
+    it 'averages actors age for the movie' do
+      expect(@marry.ave_actors_age).to eq(44)
     end
   end
 end
