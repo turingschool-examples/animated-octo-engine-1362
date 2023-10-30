2c7be322941d023908c13798b3004503845779a6
Kevin Zolman <kevin.zolman@gmail.com>
2023-10-30 09:41:46 -0600

    Write #list_movies_details and model tests in studio

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,3 +1,11 @@
 class Studio < ApplicationRecord
   has_many :movies
+
+  def list_movies_details
+    movies_details = movies.pluck(:title, :creation_year, :genre)
+
+    movies_details = movies_details.flat_map do |movie|
+      movie.join(", ")
+    end
+  end
 end
--- spec/models/studio_spec.rb
+++ spec/models/studio_spec.rb
@@ -4,4 +4,21 @@ RSpec.describe Studio, type: :model do
   describe "relationships" do
     it {should have_many :movies}
   end
+
+  before (:each) do
+    @studio_1 = Studio.create!(name: "Universal Studios", location: "Hollywood")
+    @studio_2 = Studio.create!(name: "Ramoji Film City", location: "India")
+    @movie_1 = @studio_1.movies.create!(title: "Toy Story", creation_year: "1995", genre: "animation")
+    @movie_2 = @studio_1.movies.create!(title: "Mystery Men", creation_year: "1999", genre: "action")
+    @movie_3 = @studio_2.movies.create!(title: "RRR", creation_year: "2022", genre: "action")
+    @movie_4 = @studio_2.movies.create!(title: "Aadhi", creation_year: "2018", genre: "thriller")
+  end
+
+  describe "instance methods" do
+    describe "#list_movies_details" do
+      it "returns a list of a studios's employees and their details" do
+        expect(@studio_1.list_movies_details).to eq(["Toy Story, 1995, animation", "Mystery Men, 1999, action"])
+      end
+    end
+  end
 end
