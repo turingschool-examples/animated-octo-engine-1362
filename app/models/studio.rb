48e31a5b6129928d47e4894ddc9ff5646b855766
Cory Powell <coryrpowell@gmail.com>
2023-10-30 13:32:58 -0400

    created additional associations that studios have through movies and movie_actors

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,3 +1,5 @@
 class Studio < ApplicationRecord
   has_many :movies
+  has_many :movie_actors, through: :movies
+  has_many :actors, through: :movie_actors
 end
--- spec/models/studio_spec.rb
+++ spec/models/studio_spec.rb
@@ -3,5 +3,7 @@ require "rails_helper"
 RSpec.describe Studio, type: :model do
   describe "relationships" do
     it {should have_many :movies}
+    it {should have_many :movie_actors}
+    it {should have_many :actors}
   end
 end
