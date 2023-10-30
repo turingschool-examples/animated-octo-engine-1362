9056a7fba0f926656182245dad8fb42ba8edbc68
Sam Tran <samuelttran@gmail.com>
2023-10-30 11:26:33 -0600

    Add actor validations

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Actor, type: :model do
     it { should have_many :actor_movies }
     it { should have_many(:movies).through(:actor_movies) }
   end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:age) }
+    it { should validate_numericality_of(:age) }
+  end
 end
