3ee49d26cffbf191ce8827c12132b4432181676a
Blake Sergesketter <blakesergesketter@gmail.com>
2023-10-30 10:15:20 -0500

    model relationsship testing.

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -2,6 +2,9 @@ require "rails_helper"
 
 RSpec.describe Movie, type: :model do
   describe "relationships" do
-    it {should belong_to :studio}
+    it { should belong_to :studio }
+    it { should have_many :movie_actors }
+    it { should have_many(:actors).through(:movie_actors) }
+
   end
 end
